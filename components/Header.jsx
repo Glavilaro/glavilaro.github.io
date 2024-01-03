6ad805c2bdc0e40238dd1aaf3b0c6d239ac486c0
gladys vilaro <94321396+Glavilaro@users.noreply.github.com>
2024-01-03 11:59:20 -0300

    Add files via upload

---
+++ components/Header.jsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+
+export const Header = ({
+    allProducts,
+    setAllProducts,
+    total,
+    countProducts,
+    setCountProducts,
+    setTotal,
+}) => {
+    const [active, setActive] = useState(false);
+
+    const onDeleteProduct = product => {
+        const results = allProducts.filter(
+            item => item.id !== product.id
+        );
+
+        setTotal(total - product.price * product.quantity);
+        setCountProducts(countProducts - product.quantity);
+        setAllProducts(results);
+    };
+
+    const onCleanCart = () => {
+        setAllProducts([]);
+        setTotal(0);
+        setCountProducts(0);
+    };
+
+
+    return (
+        <header>
+            <div className='container-Nav'>
+                <h1>ShopCart</h1>
+                <ul>
+                    <li><a href="#">nuestros productos</a></li>
+                </ul>
+            </div>
+            <div className='container-icon'>
+                <div
+                    className='container-cart-icon'
+                    onClick={() => setActive(!active)}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+
+                    </svg>
+                    <div className='count-products'>
+                        <span id='contador-productos'>{countProducts}</span>
+                    </div>
+                </div>
+
+                <div
+                    className={`container-cart-products ${active ? '' : 'hidden-cart'
+                        }`}
+                >
+                    {allProducts.length ? (
+                        <>
+                            <div className='row-product'>
+                                {allProducts.map(product => (
+                                    <div className='cart-product' key={product.id}>
+                                        <div className='info-cart-product'>
+                                            <span className='cantidad-producto-carrito'>
+                                                {product.quantity}
+                                            </span>
+                                            <p className='titulo-producto-carrito'>
+                                                {product.nameProduct}
+                                            </p>
+                                            <span className='precio-producto-carrito'>
+                                                ${product.price}
+                                            </span>
+                                        </div>
+                                        <svg
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            fill='none'
+                                            viewBox='0 0 24 24'
+                                            strokeWidth='1.5'
+                                            stroke='currentColor'
+                                            className='icon-close'
+                                            onClick={() => onDeleteProduct(product)}
+                                        >
+                                            <path
+                                                strokeLinecap='round'
+                                                strokeLinejoin='round'
+                                                d='M6 18L18 6M6 6l12 12'
+                                            />
+                                        </svg>
+                                    </div>
+                                ))}
+                            </div>
+
+                            <div className='cart-total'>
+                                <h3>Total:</h3>
+                                <span className='total-pagar'>${total}</span>
+                            </div>
+
+                            <button className='btn-clear-all' onClick={onCleanCart}>
+                                Vaciar Carrito
+                            </button>
+
+
+                        </>
+                    ) : (
+                        <p className='cart-empty'>El carrito está vacío</p>
+                    )}
+
+                </div>
+            </div>
+        </header >
+
+    );
+};
